2583d46198e352db188eae736875bea242698953
Никита Смоляков <nikita.smoliakov@yandex.ru>
2024-01-28 20:41:43 +0700

    add Model

---
+++ Shift_trainee/Main/Model/ModelStorage.swift
@@ -0,0 +1,8 @@
+//
+//  ModelStorage.swift
+//  Shift_trainee
+//
+//  Created by Никита on 28.01.2024.
+//
+
+import Foundation
---
+++ Shift_trainee/Main/Model/NoteModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteModel.swift
+//  Shift_trainee
+//
+//  Created by Никита on 28.01.2024.
+//
+
+import Foundation
